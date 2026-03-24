e134425b9539f9b8767bd2b5c6e9cbe7de96b8bf
jaries23 <jaries23@users.noreply.github.com>
2026-03-24 09:31:14 +0900

    Connect GitHub Pages frontend to Supabase

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.__COMMUNITY_CONFIG__ = {
-  supabaseUrl: "",
-  supabaseKey: "",
+  supabaseUrl: "https://aogbkkutpobktbtbozgt.supabase.co",
+  supabaseKey: "sb_publishable_wsTPCNEG8kzmiCTxDuNYMQ_JtIS2eEj",
 };
